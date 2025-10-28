f055335e3695fd60e38f0b6ad2c6de5dab2ca5c1
spahicadev <adis@spahica.com>
2025-10-28 10:43:22 +0100

    Small changes for documentation

--- app/index.js
+++ app/index.js
@@ -1,8 +1,26 @@
 import express from "express";
 
 const app = express();
+// app.use(express.json());
 
 const PORT = 8080;
+
+app.get("/", (req, res) => {
+  const payload = {
+    name: "Adis",
+  };
+  // res.status(200).json(payload);
+  res.status(202).write(JSON.stringify(payload));
+  res.end();
+});
+
+app.post("/user/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  // console.log(req.body); JSON.parse ne radi je je req.body undefined dok ne definiram app.use express.json()
+  res.status(201).write("OK");
+  res.end();
+});
 
 app.listen(PORT, (err) => {
   if (err) {
@@ -11,3 +29,6 @@ app.listen(PORT, (err) => {
     console.log(`Backend started at port ${PORT}`);
   }
 });
+
+//express parsa vec json u response
+//endpoint doslovno funkcija koja lezi na toj ruti/endpointu
