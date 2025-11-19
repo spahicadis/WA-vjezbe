48048a4fe1a0db8220828404c6d3b69609490309
spahicadev <adis@spahica.com>
2025-11-19 08:26:13 +0100

    Repo structure

--- WA2.1_materijali/utils/pizzeHelpers.js
+++ WA2.1_materijali/utils/pizzeHelpers.js
@@ -5,6 +5,6 @@
   for (const [key, value] of Object.entries(req.body)) {
     const check = allowedKeys.some((allowedKeys) => allowedKeys === key);
     if (!check) {
-      return res.status(400).json({ message: "Ključevi nisu dobri." });
+      return res.status(400).json({ message: "Ključevi nisu dobri." });//throw Error
     }
   }*/
